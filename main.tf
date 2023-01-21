e8b1a70e97b996249bfb0675fe54d5ed71e85c0c
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-01-21 20:27:00 +0100

    cleanup

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "random_password" "k3s_token" {
   length  = 48
   special = false
 }
+
+resource "hcloud_ssh_key" "default" {
+  name       = var.my_public_ssh_name
+  public_key = var.my_public_ssh_key
+}
