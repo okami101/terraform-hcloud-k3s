d1ea0ee74a6dc963f91a1b8dbcfb3c397378e1cb
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2023-08-20 17:08:00 +0200

    expose all hetzner resource

--- output.tf
+++ output.tf
@@ -8,19 +8,19 @@ output "firewall_private_id" {
   description = "ID of the private firewall, allowing attaching to any custom servers"
 }
 
-output "lb_ids" {
-  value       = { for i, lb in hcloud_load_balancer.lbs : i => lb.id }
-  description = "Hetzner Identifier of load balancers, use them to configure services"
+output "lbs" {
+  value       = hcloud_load_balancer.lbs
+  description = "Hetzner load balancers, use them to configure services"
 }
 
-output "controller_ids" {
-  value       = [for s in local.servers : hcloud_server.servers[s.name].id if s.role == "controller"]
+output "controllers" {
+  value       = [for s in local.servers : hcloud_server.servers[s.name] if s.role == "controller"]
   description = "Hetzner Identifier of controller servers"
 }
 
-output "worker_ids" {
+output "workers" {
   value = { for n in var.agent_nodepools :
-    n.name => [for s in local.servers : hcloud_server.servers[s.name].id if s.role == n.name]
+    n.name => [for s in local.servers : hcloud_server.servers[s.name] if s.role == n.name]
   }
   description = "Hetzner Identifier of workers grouped by role"
 }
